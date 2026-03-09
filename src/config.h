b64d45aecc6b5504708d8bf361ad0f8caa16b82f
Elliot Lu <ellilu1191@gmail.com>
2026-03-09 17:13:29 -0400

    Enhance mainLoop()

--- src/config.h
+++ src/config.h
@@ -6,7 +6,7 @@
 #include <type_traits>
 #include "phys.h"
 
-enum class Mode { FMM, FMMDIR };
+enum class Mode { FMM, DIR, FMMDIR };
 
 enum class Precision { VERYLOW, LOW, MEDIUM, HIGH, VERYHIGH };
 
@@ -52,7 +52,7 @@ std::string getModeStr(Mode mode) {
     return [&]() {
         switch (mode) {
             case Mode::FMM:    return "FMM";
-            // case Mode::DIR:    return "DIRECT";
+            case Mode::DIR:    return "DIRECT";
             case Mode::FMMDIR: return "FMM+DIRECT";
         };
         } ();
